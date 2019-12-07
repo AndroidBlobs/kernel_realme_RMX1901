2302876f1ffe0fe3702b347226d5d03c282f54d8
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-07 12:15:58 +0000

    Add Headers modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -117,6 +117,7 @@ struct mmc_request {
 
 	/* Allow other commands during this ongoing data transfer or busy wait */
 	bool			cap_cmd_during_tfr;
+        //yh@PSW.BSP.Storage.Emmc, 2018-09-30, Add for monitor cmdq driver wait time
 	ktime_t			io_start;
 #ifdef CONFIG_BLOCK
 	int			lat_hist_enabled;
